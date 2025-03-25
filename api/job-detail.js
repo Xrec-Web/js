ad80b51369f03373f55412c7659bf2d8a5311801
Xrec-Web <web@xrecruiter.io>
2025-03-26 09:47:06 +1000

    Update job-detail.js

--- api/job-detail.js
+++ api/job-detail.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
   
   // Replace with your Loxo credentials
   const AGENCY_SLUG = 'rover-recruitment';
-  const BEARER_TOKEN = 'YOUR_BEARER_TOKEN';
+  const BEARER_TOKEN = '8f4998dbcf4615d2c28f8063040d916e49e44d2aca927b5abbef53d7746754e31e49f4aa385ff40368ec86ec1b1e95fddd23c5cb0e7f349259eab2ff83ec9f0f70185b2c56c962e1f432c619a1dad40c3bf76e157c6a6d18c9521452e8d72390c4de9e496fa87236728b9a77cb8a7bd5a0334f795745700526fdc83eb68a3afa';
   
   try {
     const response = await fetch(`https://app.loxo.co/api/${AGENCY_SLUG}/jobs/${id}`, {
